5d72968e498fcc88232811e94fc70aa5db623282
abcnkcabc <11302010049@fudan.edu.cn>
2014-06-05 20:42:28 +0800

    controller

---
+++ src/Controller/QueryControl.java
@@ -0,0 +1,35 @@
+package Controller;
+
+public class QueryControl implements QueryController {
+
+	@Override
+	public int getTotalWordNum(String libname) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getRecitedWordNum(String libname) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getRightWordNum(String libname) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getWrongWordNum(String libname) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getRightRate(String libname) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
